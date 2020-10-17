74a7c238eaee9f9ffb260c37417d1704a42784ce
wyang19a <wyang19a@gmail.com>
2020-10-17 22:19:46 +0900

    initial commit

---
+++ schemas/person.js
@@ -0,0 +1,40 @@
+import { MdPerson as icon } from 'react-icons/md';
+
+export default {
+  // Computer Name
+  name: 'person',
+  // visible title
+  title: 'Slicemasters',
+  type: 'document',
+  icon, // () => '💩',
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: '100',
+      },
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+      description: 'Tell us about them',
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+  ],
+};
